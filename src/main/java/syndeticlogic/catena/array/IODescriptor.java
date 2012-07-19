5fb8950814140fb642e8494aacf29d70ed6885f0
James Percent <james@syndeticlog.org>
2012-07-19 16:04:38 -0400

    Refactored IODescriptor

--- src/main/java/syndeticlogic/catena/array/IODescriptor.java
+++ src/main/java/syndeticlogic/catena/array/IODescriptor.java
@@ -16,15 +16,12 @@
  */
 package syndeticlogic.catena.array;
 
-import java.util.LinkedList;
-import syndeticlogic.catena.array.SegmentCursor;
 /**
  * @author <a href="mailto:james@empty-set.net">James Percent</a>
  */
 public class IODescriptor {
 
-//    private static final Log log = LogFactory.getLog(IODescriptor.class);
-    private final ValueSizeRecorder valueSizeRecorder; 
+    private final ValueRecorder valueSizeRecorder; 
     private final byte[] buffer;
     private final int offset;
     
@@ -37,7 +34,6 @@ public class IODescriptor {
         }
         this.buffer = buffer;
         this.offset = offset;
-//        this.values = n;
     }
 
     public int size(int i) {
@@ -62,80 +58,5 @@ public class IODescriptor {
     
     public int offset() {
         return offset;
-    }
-        
-    public interface ValueSizeRecorder {
-        int valuesScanned();
-        int recordValuesScanned(SegmentCursor cursor);
-        int valueScannedSize(int i);
-    }
-    
-    public class FixedLengthRecorder implements ValueSizeRecorder {
-        private int size;
-        private int scanned;
-        
-        public FixedLengthRecorder(int size) {
-            this.size = size;
-            this.scanned = 0;
-        }
-        
-        public int recordValuesScanned(SegmentCursor cursor) {
-            int remainingBytes = cursor.remaining();
-            assert remainingBytes % size == 0;
-            assert remainingBytes >= 0;
-            int newValues = remainingBytes / size;
-            scanned += newValues;
-            return newValues * size;
-        }
-        
-        public int valuesScanned() {
-            return scanned;
-        }
-        
-        public int valueScannedSize(int i) {
-            if(i >= scanned) {
-                return -1;
-            } else {
-                return size;
-            }
-        }
-    }
-    
-    public class VariableLengthRecorder implements ValueSizeRecorder {
-        private ArrayDescriptor arrayDescriptor;
-        private LinkedList<Integer> recordedSizes;
-        private int start;
-        
-        public VariableLengthRecorder(ArrayDescriptor arrayDescriptor, int startIndex) {
-            this.arrayDescriptor = arrayDescriptor;
-            this.start = startIndex;
-        }
-        
-        public int recordValuesScanned(SegmentCursor cursor) {
-            int remainingBytes = cursor.remaining();
-            int scanSize = 0;
-            for(int i = start + recordedSizes.size(); remainingBytes > 0; i++) {
-                int nextValueSize = arrayDescriptor.valueSize(i);
-                remainingBytes -= nextValueSize;
-                if(remainingBytes < 0) {
-                    break;
-                }
-                recordedSizes.add(nextValueSize);
-                scanSize += nextValueSize;
-            }
-            return scanSize;
-        }
-        
-        public int valuesScanned() {
-            return recordedSizes.size();
-        }
-        
-        public int valueScannedSize(int i) {
-            int ret = -1;
-            if(i < recordedSizes.size()) {
-                ret = recordedSizes.get(i);
-            }
-            return ret;
-        }
-    }
+    }  
 }
