cb23d25f8d1540145db08091cebde32ef1034802
James Percent <james@syndeticlog.org>
2012-10-14 02:06:42 -0400

    Removed old files

--- src/main/java/syndeticlogic/catena/array/Array.java
+++ src/main/java/syndeticlogic/catena/array/Array.java
@@ -1,160 +1,47 @@
-/*
- *   Copyright 2010 - 2012 James Percent
- *
- *   This program is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 package syndeticlogic.catena.array;
+
+import java.nio.ByteBuffer;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import syndeticlogic.catena.predicate.Predicate;
-import syndeticlogic.catena.store.Segment;
+import syndeticlogic.catena.type.Value;
 import syndeticlogic.catena.utility.Transaction;
-import syndeticlogic.catena.array.SegmentCursor;
 
-/**
- * @author <a href="mailto:james@empty-set.net">James Percent</a>
- */
-public class Array {
-
-    public enum LockType { ReadLock, WriteLock };  
-    private static final Log log = LogFactory.getLog(Array.class);
-    private ArrayDescriptor arrayDescriptor;
-    private SegmentController segmentController;
-    private SegmentCursor segmentCursor;
-    private int index;
-    private boolean configured;
-    
-    Array(ArrayDescriptor descriptor, SegmentController segmentController) {
-        assert descriptor != null;
-        this.arrayDescriptor = descriptor;
-        this.segmentController = segmentController;
-        this.index = 0;
-        this.segmentCursor = new SegmentCursor();
-    }
-
-    public IODescriptor scan(IODescriptor ioDescriptor) {
-        if (!configured) { return null; }
-        if (log.isTraceEnabled()) { log.debug("pos=" + index); }
-
-        int size = ioDescriptor.ioSize();
-        int offset = ioDescriptor.offset();
-        int accumulation = 0;
-        boolean moreToScan = true;
-
-        while (accumulation < size && moreToScan) {
-            int scanSize = ioDescriptor.recordValuesScanned(segmentCursor);
-            int scanned = segmentCursor.scan(ioDescriptor.buffer(), offset, scanSize);
-            assert scanSize == scanned;
-            accumulation += scanned;
-            offset += scanned;
-
-            if (accumulation < size) {
-                moreToScan = segmentController.unlockAndLockNextSegment(segmentCursor);
-            }
-        }
-
-        if (moreToScan && segmentCursor.scanned()) {
-            moreToScan = segmentController.unlockAndLockNextSegment(segmentCursor);
-        }
-
-        if (!moreToScan) {
-            configured = false;
-        }
-
-        index += ioDescriptor.valuesScanned();
-        return ioDescriptor;
-    }
-
-    public void update(byte[] buffer, int offset) {
-        assert arrayDescriptor.isFixedLength();
-        update(buffer, offset, arrayDescriptor.typeSize());
-    }
-
-    public void update(byte[] buffer, int offset, int size) {
-        assert configured;
-        int oldSize = arrayDescriptor.update(index, size);
-        segmentCursor.update(buffer, offset, oldSize, size);
-    }
-
-    public void append(byte[] buffer, int offset) {
-        assert arrayDescriptor.isFixedLength();
-        append(buffer, offset, arrayDescriptor.typeSize());
-    }
-
-    public void append(byte[] buffer, int offset, int size) {
-        assert buffer.length - offset >= size && size > 0;
-        arrayDescriptor.acquire();
-        try {
-            arrayDescriptor.append(size);
-            position(arrayDescriptor.length() - 1, LockType.WriteLock);
-        } finally {
-            arrayDescriptor.release();
-        }
-        segmentCursor.append(buffer, offset, size);
-        index++;
-        complete(LockType.WriteLock);
-    }
-
-    public void delete() {
-        int size = arrayDescriptor.delete(index);
-        segmentCursor.delete(size);
-        index--;
-    }
-
-    public void complete(LockType lt) {
-        if (configured) {
-            segmentController.unlockSegment(segmentCursor, lt);
-            configured = false;
-        }
-    }
-
-    public void commit() {
-        arrayDescriptor.acquire();
-        for (Segment s : arrayDescriptor.segments()) {
-            s.commit();
-        }
-        arrayDescriptor.persist();
-        arrayDescriptor.release();
-    }
-
-    public void configure(Predicate p, Transaction t) {
-        configured = true;
-    }
-    
-    public ArrayDescriptor descriptor() {
-        return arrayDescriptor;
-    }
-
-    public int position() {
-        return index;
-    }
-
-    public void position(int position, LockType lockType) {
-        long offset = arrayDescriptor.convertIndexToOffset(position);
-        segmentController.findAndLockSegment(segmentCursor, lockType, offset);
-        index = position;
-        configured = true;
-    }
-
-    public boolean hasMore() {
-        return (index == (arrayDescriptor.length()) ? false : true);
-    }
-
-    public IODescriptor createIODescriptor(byte[] buffer, int offset) {
-        return new IODescriptor(arrayDescriptor, index, buffer, offset);
-    }
-  
+public class Array<Type extends Value> {
+    private final Log log = LogFactory.getLog(Array.class);
+    private final static int DEFAULT_CHUNK_SIZE = 1048576;
+	private BinaryArray binaryArray;
+	private Predicate predicate;
+	private Transaction transaction;
+	private ByteBuffer chunk;
+	
+	Array(BinaryArray array, Transaction t, Predicate p) {
+		this.binaryArray = array;
+		this.transaction = t;
+		this.predicate = p;
+		this.chunk = ByteBuffer.allocate(DEFAULT_CHUNK_SIZE);
+	}
+	
+	public void setPredicate(Predicate predicate) {
+	    this.predicate = predicate;
+	}
+	
+	Type[] scan() {
+	    int size = binaryArray.remaining();
+	    //binaryArray.
+	}
+	
+	void update(Predicate p) {
+	    
+	}
+	
+	void delete() {
+	    
+	}
+	
+	void append(byte[] bytes) {
+	    
+	}
 }
