2e573260ec584ebeecfd7370340eaaa9bc61337d
James Percent <james@syndeticlog.org>
2012-10-14 02:02:08 -0400

    Added event listening functionality to the DynamicProperties

--- src/main/java/syndeticlogic/catena/utility/DynamicProperties.java
+++ src/main/java/syndeticlogic/catena/utility/DynamicProperties.java
@@ -11,8 +11,9 @@ import org.apache.commons.vfs2.FileChangeEvent;
 
 public class DynamicProperties extends FileSystemWatcher {
     private static final Log log = LogFactory.getLog(DynamicProperties.class); 
+    private Set<PropertyEventListener> listeners;
+    private volatile Properties properties;
     private Set<URL> files;
-    volatile Properties properties;
     
     public DynamicProperties(Properties baseProperties, String directory) {
         this(directory);
@@ -37,9 +38,7 @@ public class DynamicProperties extends FileSystemWatcher {
     public void updateFiles(FileChangeEvent event) {
         try {
             URL url = event.getFile().getURL();
-            synchronized (this) {
-                files.add(url);
-            }
+
             Properties newProps = PropertiesUtility.load(event.getFile().getURL());
             if (newProps == null) {
                 return;
@@ -48,6 +47,12 @@ public class DynamicProperties extends FileSystemWatcher {
             Set<Entry<Object, Object>> props = newProps.entrySet();
             for (Entry<Object, Object> prop : props) {
                 properties.put(prop.getKey(), prop.getValue());
+            }
+            synchronized (this) {
+                files.add(url);
+                for(PropertyEventListener listener : listeners) {
+                    listener.eventOccured();
+                }
             }
         } catch (Exception e) {
             log.error("Exception on file changed event " + e, e);
@@ -61,6 +66,12 @@ public class DynamicProperties extends FileSystemWatcher {
     public URL[] files() {
         synchronized(this) {
             return (URL[])files.toArray();
+        }
+    }
+    
+    public void addPropertyEventListener(PropertyEventListener listener) {
+        synchronized(this) {
+            listeners.add(listener);
         }
     }
 }
