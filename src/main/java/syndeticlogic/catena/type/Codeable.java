865da59791d0f1f0e19aa51db7d0a84bb2731304
James Percent <james@syndeticlog.org>
2012-10-21 14:20:41 -0400

    Beautification

--- src/main/java/syndeticlogic/catena/type/Codeable.java
+++ src/main/java/syndeticlogic/catena/type/Codeable.java
@@ -1,11 +1,11 @@
 package syndeticlogic.catena.type;
 
 public interface Codeable extends Comparable<Codeable>  {
-	String oridinal();
 	int size();
+    String oridinal();
 	int encode(byte[] dest, int offset);
 	int decode(byte[] source, int offset);
-	int hashCode();
-	boolean equals(Object obj);
 	int compareTo(Codeable c);
+    boolean equals(Object obj);
+    int hashCode();
 }
