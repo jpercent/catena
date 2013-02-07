2f80386f6c14cefd81db24fa6e9a92d15ae7473d
James Percent <james@syndeticlogic.org>
2013-02-06 23:54:15 -0500

    Created a new base value ScatterGatherValue that extends Value. Modified variable length values (BinaryValue, CodeableValue and StringValue) inherit from the new base class.

--- src/main/java/syndeticlogic/catena/type/BinaryValue.java
+++ src/main/java/syndeticlogic/catena/type/BinaryValue.java
@@ -1,12 +1,14 @@
 package syndeticlogic.catena.type;
 
-public class BinaryValue extends Value {
+public class BinaryValue extends ScatterGatherValue {
 
     public BinaryValue() {
+        super();
     }
     
 	public BinaryValue(byte[] data, int offset, int length) {
-	    super(data, offset, length);
+	    super();
+	    add(data, offset, length);
 	}
 	
     @Override
@@ -20,27 +22,36 @@ public class BinaryValue extends Value {
         boolean greater = false;
         boolean equal = true;
         boolean less = false;
-        
+        int rawBytesIter = offset;
+        int myLen = 0;
         int theirLen = length;
-        int myLen = this.length;
-        
-        for(int i = this.offset, j = offset, k = 0; 
-            k < myLen && k < theirLen; i++, j++, k++) 
-        {
-            if(this.data[i] < rawBytes[j]) { 
-                less = true;
-                equal = false;
-                break;
-            } else if(this.data[i] > rawBytes[j]) {
-                greater = true;
-                equal = false;
-                break;
-            } else if(this.data[i] != rawBytes[j]) {
-                equal = false;
+        int cumLen = 0;
+
+        for (PartialValue value : values) {
+            int segmentLen = value.length;
+            int count = 0;
+            for (int dataIter = value.offset; count < segmentLen && cumLen < theirLen; dataIter++, rawBytesIter++, count++, cumLen++) {
+                if (value.data[dataIter] < rawBytes[rawBytesIter]) {
+                    less = true;
+                    equal = false;
+                    break;
+                } else if (value.data[dataIter] > rawBytes[rawBytesIter]) {
+                    greater = true;
+                    equal = false;
+                    break;
+                } else if (value.data[dataIter] != rawBytes[rawBytesIter]) {
+                    equal = false;
+                    break;
+                }
+            }       
+            
+            if(!equal) { 
+                assert greater || less;
                 break;
             }
+            myLen += count;
         }
-
+        
         if(equal && theirLen < myLen) {
             equal = false;
             greater = true;
@@ -48,7 +59,7 @@ public class BinaryValue extends Value {
             less = true;
             equal = false;
         }
-        
+
         assert (greater && !(less || equal)) 
             || (less && !(greater || equal)) 
             ||(equal && !(less || greater)); 
---
+++ src/main/java/syndeticlogic/catena/type/ScatterGatherValue.java
@@ -0,0 +1,71 @@
+package syndeticlogic.catena.type;
+
+import java.util.LinkedList;
+
+public abstract class ScatterGatherValue extends Value {
+    protected final LinkedList<PartialValue> values;
+    private boolean cache;
+    
+    public ScatterGatherValue() {
+        super(null, 0, 0);
+        values = new LinkedList<PartialValue>();
+        length = 0;
+        cache = false;
+    }
+    
+    public void add(byte[] data, int offset, int length) {
+        values.add(new PartialValue(data, offset, length));
+        this.length += length;
+    }
+    
+    public byte[] gather() {
+        byte[] retdata = new byte[length];
+        int retoffset = 0;
+        for(PartialValue value : values) {
+            System.arraycopy(value.data, value.offset, retdata, retoffset, value.length);
+            retoffset += value.length;
+        }
+       return retdata;
+    }
+    
+    public void cacheGather(boolean cache) {
+        this.cache = true;
+    }
+    
+    public boolean cacheGather() {
+        return this.cache;
+    }
+    
+    @Override
+    public void reset(byte[] data, int offset, int length) {
+        values.clear();
+        length = 0;
+        add(data, offset, length);
+    }
+    
+    @Override
+    public byte[] data() {
+        return gather();
+    }
+    
+    @Override
+    int offset() {
+        return 0;
+    }
+    
+    @Override
+    int length() {
+        return length;
+    }
+
+    protected class PartialValue {
+        public PartialValue(byte[] d, int o, int l) {
+            this.data = d;
+            this.offset = o;
+            this.length = l;
+        }
+        public byte[] data;
+        public int offset;
+        public int length;
+    }
+}
