c69cda3bd88da2017a5958a331a86d0010d432ad
James Percent <james@syndeticlogic.org>
2013-04-18 03:52:30 -0400

    Test cleanup

--- src/test/java/syndeticlogic/catena/store/SegmentHeaderTest.java
+++ src/test/java/syndeticlogic/catena/store/SegmentHeaderTest.java
@@ -5,8 +5,6 @@ import static org.junit.Assert.*;
 import java.io.RandomAccessFile;
 import java.nio.channels.FileChannel;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 import syndeticlogic.catena.stubs.PageManagerStub;
@@ -19,8 +17,8 @@ public class SegmentHeaderTest {
      SegmentHeader segmentHeader;
      String filename = "target"+System.getProperty("file.separator")+"segmentHeaderTest";
      
-    @Before
-    public void setUp() throws Exception {
+    @Test
+    public void testSegmentHeader() throws Exception {
         pageManager = new PageManagerStub();
         
         RandomAccessFile file = new RandomAccessFile(filename, "rw");
@@ -28,27 +26,17 @@ public class SegmentHeaderTest {
         fileChannel.truncate(0);
         segmentHeader = new SegmentHeader(fileChannel, pageManager);
         Codec.configureCodec(null);
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        fileChannel.truncate(0);
-        fileChannel.force(true);
-        pageManager = null;
-        segmentHeader = null;
-        fileChannel = null;
-    }
-
-    @Test
-    public void testSegmentHeader() throws Exception {
+        
         segmentHeader.type(Type.BINARY);
-        segmentHeader.dataSize(22L);
+        segmentHeader.dataSize(22);
         segmentHeader.pages(44);
         segmentHeader.store();
         
         fileChannel.force(true);
         fileChannel.close();
-        RandomAccessFile file = new RandomAccessFile(filename, "rw");
+        file.close();
+        
+        file = new RandomAccessFile(filename, "rw");
         fileChannel = file.getChannel();
         segmentHeader = new SegmentHeader(fileChannel, pageManager);
         
@@ -56,5 +44,12 @@ public class SegmentHeaderTest {
         assertEquals(Type.BINARY, segmentHeader.type());
         assertEquals(22L, segmentHeader.dataSize());
         assertEquals(44, segmentHeader.pages());
+        
+        fileChannel.truncate(0);
+        fileChannel.force(true);
+        pageManager = null;
+        segmentHeader = null;
+        fileChannel = null;
+        file.close();
     }
 }
