45d0f01a48970118adee18c5ba2e2049f12ff537
James Percent <james@syndeticlogic.org>
2013-04-18 06:24:15 -0400

    Added InvertedList and InvertedListTest

---
+++ src/main/java/syndeticlogic/catena/text/InvertedList.java
@@ -0,0 +1,231 @@
+package syndeticlogic.catena.text;
+
+import java.nio.ByteBuffer;
+
+import syndeticlogic.catena.type.Codeable;
+import syndeticlogic.catena.type.Type;
+import syndeticlogic.catena.utility.Codec;
+
+public class InvertedList implements Codeable {
+    private static int PAGE_SIZE = 8192;
+    private int wordId;
+    private String word;
+    private int documentFrequency;
+    private int[][] documentIds;
+    private int slots;
+    private int slotCursor;
+    private int pageCursor = 0;
+    private int slotIterator = 0;
+    private int pageIterator = 0;
+    
+    public InvertedList() {
+        this.wordId = -1;
+        this.documentFrequency = 0;
+        documentIds = new int[PAGE_SIZE][];
+        documentIds[0] = new int[PAGE_SIZE];
+        slots = PAGE_SIZE;
+        slotCursor = 0;
+        pageCursor = 0;
+        word = null;
+    }
+    
+    public InvertedList(int wordId) {
+        this.wordId = wordId;
+        this.documentFrequency = 0;
+        documentIds = new int[PAGE_SIZE][];
+        documentIds[0] = new int[PAGE_SIZE];
+        slots = PAGE_SIZE;
+        slotCursor = 0;
+        pageCursor = 0;
+        word = null;
+    }
+    
+    public void addDocumentId(int docId) {
+        if(pageCursor == PAGE_SIZE) {
+            addPage();
+        }
+        documentIds[slotCursor][pageCursor] = docId;
+        documentFrequency++;
+        pageCursor++;
+    }
+
+    private void addPage() {
+        if(slotCursor + 1 == slots) {
+            expand();
+        }
+        slotCursor++;
+        pageCursor = 0;
+        documentIds[slotCursor] = new int[PAGE_SIZE];
+    }
+
+    private void expand() {
+        int[][] newDocs = new int[slots+PAGE_SIZE][];
+        System.arraycopy(documentIds, 0, newDocs, 0, slots);
+        slots += PAGE_SIZE;
+        documentIds = newDocs;
+    }
+            
+    @Override
+    public int encode(byte[] dest, int offset) {
+        int copied = 0;
+        Codec.getCodec().encode(wordId, dest, offset+copied);
+        copied += Type.INTEGER.length();
+        
+        Codec.getCodec().encode(documentFrequency, dest, offset+copied);
+        copied += Type.INTEGER.length();
+       
+        int docIdsSize = documentIdsSize();
+        Codec.getCodec().encode(docIdsSize, dest, offset+copied);
+        copied += Type.INTEGER.length();
+     
+        int computedDocSize = flattenTable(dest, offset+copied);
+        assert computedDocSize == docIdsSize;
+        copied += computedDocSize;
+        return copied;
+    }
+    
+    private int flattenTable(byte[] dest, int offset) {
+        int copied = 0;
+        for(int i = 0; i <= slotCursor; i++) {
+            int length = PAGE_SIZE;
+            if(i == slotCursor) {
+                length = pageCursor;
+            }
+            ByteBuffer transferBuff = ByteBuffer.wrap(dest, offset+copied, length*Type.INTEGER.length());
+            transferBuff.asIntBuffer().put(documentIds[i], 0, length);
+            copied += length*Type.INTEGER.length();
+        }
+        return copied;
+    }
+    
+    @Override
+    public int decode(byte[] source, int offset) {
+        int copied = 0;
+        wordId = Codec.getCodec().decodeInteger(source, offset+copied);
+        copied += Type.INTEGER.length();
+        
+        documentFrequency = Codec.getCodec().decodeInteger(source, offset+copied);
+        copied += Type.INTEGER.length();
+        
+        int docSize = Codec.getCodec().decodeInteger(source, offset+copied);
+        copied += Type.INTEGER.length();
+        int tableSize = constructTable(source, offset+copied, docSize);
+        assert docSize == tableSize;
+        return copied+tableSize;
+    }
+
+    private int constructTable(byte[] source, int offset, int remaining) {
+        int accumulation = 0;
+        while (remaining > 0) {
+            int blockSize = PAGE_SIZE;
+            if(remaining/Type.INTEGER.length() < blockSize) {
+                blockSize = remaining/Type.INTEGER.length();
+            }
+            pageCursor = blockSize;
+            ByteBuffer transferBuf = ByteBuffer.wrap(source, offset+accumulation, remaining); 
+            transferBuf.asIntBuffer().get(documentIds[slotCursor], 0, blockSize);
+            if(pageCursor == PAGE_SIZE) {
+                addPage();
+            }
+            remaining -= blockSize*Type.INTEGER.length();
+            accumulation += blockSize*Type.INTEGER.length();
+        }
+        assert remaining == 0;
+        return accumulation;
+    }
+    
+    @Override
+    public int compareTo(Codeable c) {
+        InvertedList o = (InvertedList) c;
+        if (word == null || "".equals(word) || o.word == null || "".equals(word) ) {
+            throw new RuntimeException("compareTo requires that word values have been set");
+        }
+        int compareResult = word.compareTo(o.word);
+        return compareResult;
+    }
+    
+    @Override
+    public int size() {
+        int size = 3*Type.INTEGER.length();
+        size += documentIdsSize();
+        return size;
+    }
+    
+    private int documentIdsSize() {
+        return (/* filled up pages + last, partial page */ Type.INTEGER.length() * slotCursor * PAGE_SIZE) 
+        + (Type.INTEGER.length() * pageCursor);
+    }
+    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + documentFrequency;
+        result = prime * result + wordId;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        InvertedList other = (InvertedList) obj;
+        if (documentFrequency != other.documentFrequency)
+            return false;
+        if (wordId != other.wordId)
+            return false;
+        return true;
+    }
+    
+    public void resetIterator() {
+        slotIterator = 0;
+        pageIterator = 0;
+    }
+    
+    public boolean hasNext() {
+        if(slotIterator <= slotCursor) {
+            return true;
+        } else if(pageIterator < pageCursor) {
+            return true;
+        }
+        return false;
+    }
+    
+    public int advanceIterator() {
+        int ret = documentIds[slotIterator][pageIterator++];
+        if(pageIterator == PAGE_SIZE) {
+            slotIterator++;
+            pageIterator=0;         
+        }
+        return ret;
+    }
+    
+    public int getWordId() {
+        return wordId;
+    }
+    
+    public void setWord(String word) {
+        this.word = word;
+    }
+    
+    public String getWord() {
+        return word;
+    }
+    
+    @Override
+    public String oridinal() {
+        throw new RuntimeException("ordinal is unsupported");
+    }
+    
+    public static int getPageSize() {
+        return PAGE_SIZE;
+    }
+    
+    public static void setPageSize(int pageSize) {
+        InvertedList.PAGE_SIZE = pageSize;
+    }
+}
---
+++ src/test/java/syndeticlogic/catena/text/InvertedListTest.java
@@ -0,0 +1,118 @@
+package syndeticlogic.catena.text;
+
+import static org.junit.Assert.*;
+
+import java.util.Random;
+import org.junit.Test;
+
+public class InvertedListTest {
+
+    @Test
+    public void testAddDocumentId() {
+        testBase(512, 512, 0, 30, false);
+    }
+    
+    @Test
+    public void testCoding() {
+        testBase(512, 512, 31, 33, true);
+    }
+    
+    @Test
+    public void testCoding1() {
+        testBase(1023, 4096, 32, 21, true);
+    }
+    
+    @Test
+    public void testCoding2() {
+        testBase(511, 4095, 768, 88, true);
+    }
+    
+    @Test
+    public void testCoding3() {
+        testBase(4096, 511, 0, 4, true);
+    }
+    
+    @Test
+    public void testCoding4() {
+        testBase(4095, 1023, 0, 8, true);
+    }
+    
+    @Test
+    public void testCompare() {
+        
+        InvertedList posting = new InvertedList(12);
+        InvertedList posting1 = new InvertedList(12);
+        boolean e = false;
+        try {
+            assertEquals(-1, posting.compareTo(posting1));
+        } catch(RuntimeException re) {
+            e = true;
+        }
+        assertTrue(e);
+        e = false;
+        posting.setWord("za");
+        try {
+            assertEquals(-1, posting.compareTo(posting1));
+        } catch(RuntimeException re) {
+            e = true;
+        }
+        assertTrue(e);
+        posting1.setWord("zb");
+        e = false;
+        try {
+            assertEquals(-1, posting.compareTo(posting1));
+        } catch(RuntimeException re) {
+            e = true;
+        }
+        assertFalse(e);
+        
+        InvertedList posting2 = new InvertedList(12);
+        posting2.setWord("ya");
+        
+        InvertedList posting3 = new InvertedList(12);
+        posting3.setWord("yg");
+        
+        InvertedList posting4 = new InvertedList(12);
+        posting4.setWord("ygg");
+        
+        assertTrue(0 > posting.compareTo(posting1));
+        assertEquals(0, posting1.compareTo(posting1));
+        assertTrue(0 < posting1.compareTo(posting));
+        assertTrue(0 < posting1.compareTo(posting2));
+        assertTrue(0 > posting2.compareTo(posting3));
+        assertTrue(0 < posting1.compareTo(posting3));
+        assertTrue(0 < posting4.compareTo(posting3));        
+    }
+    
+    public void testBase(int postingsPageSize, int postings1PageSize, int offset, int postingsFactor, 
+            boolean compressDecompress) 
+    {
+        Random rand = new Random(1337);
+        InvertedList.setPageSize(postingsPageSize);
+        InvertedList posting = new InvertedList(12);
+        int size = InvertedList.getPageSize() * InvertedList.getPageSize() * postingsFactor;
+        
+        for(int i = 0; i < size; ++i) {
+            posting.addDocumentId(rand.nextInt());
+        }
+        
+        InvertedList posting1 = posting;
+        if(compressDecompress) {
+            byte[] encoded = new byte[posting.size()+offset];
+            posting.encode(encoded, offset);
+            InvertedList.setPageSize(postings1PageSize);        
+            posting1 = new InvertedList(-1);
+            posting1.decode(encoded, offset);
+        }
+        
+        rand = new Random(1337);
+        posting1.resetIterator();
+        for(int i = 0; i < size; ++i) {  
+            assertTrue(posting1.hasNext());
+            int expected = rand.nextInt();
+            int actual = posting1.advanceIterator();
+            //System.out.println("I = "+i+" expected "+expected+ " actual "+actual);
+            assertEquals(expected, actual);
+        }
+    }
+}
