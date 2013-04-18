998f24aa46bb8639ae0dd0b050252c65b8661f45
James Percent <james@syndeticlogic.org>
2013-04-18 06:20:31 -0400

    Added Array - Value-based decorator for BinaryArray.

--- src/main/java/syndeticlogic/catena/array/Array.java
+++ src/main/java/syndeticlogic/catena/array/Array.java
@@ -1,48 +1,39 @@
 package syndeticlogic.catena.array;
 
-import java.nio.ByteBuffer;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import syndeticlogic.catena.predicate.Predicate;
+import syndeticlogic.catena.array.BinaryArray.LockType;
 import syndeticlogic.catena.type.Value;
-import syndeticlogic.catena.utility.Transaction;
+import syndeticlogic.catena.utility.CompositeKey;
 
-public class Array<Type extends Value> {
+public class Array<T extends Value> {
     private final Log log = LogFactory.getLog(Array.class);
-    private final static int DEFAULT_CHUNK_SIZE = 1048576;
-	private BinaryArray binaryArray;
-	private Predicate predicate;
-	private Transaction transaction;
-	private ByteBuffer chunk;
+    CompositeKey key; 
+    BinaryArray array;
+    int retryLimit = 2;
+    
+    public Array(BinaryArray backingArray) {
+        array = backingArray;
+    }
 	
-	Array(BinaryArray array, Transaction t, Predicate p) {
-		this.binaryArray = array;
-		this.transaction = t;
-		this.predicate = p;
-		this.chunk = ByteBuffer.allocate(DEFAULT_CHUNK_SIZE);
-	}
-	
-	public void setPredicate(Predicate predicate) {
-	    this.predicate = predicate;
-	}
-	
-	Type[] scan() {
-	    int size = binaryArray.remaining();
+    public void position(int position, LockType lockType) {
+        array.position(position, lockType);
+    }
+    
+	public T[] scan() {
 	    return null;
-	    //binaryArray.
 	}
 	
-	void update(Predicate p) {
-	    
+	public void append(T t) {
+	    array.append(t.data(), t.offset(), t.length());
 	}
 	
-	void delete() {
-	    
+	public void commit() {
+	    array.commit();
 	}
 	
-	void append(byte[] bytes) {
-	    
+	public void complete(LockType lockType) {
+	    array.complete(lockType);
 	}
 }
