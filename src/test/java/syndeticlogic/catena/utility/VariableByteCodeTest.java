0fa416a19b6ff69aa1355f727bb48b8ebafa875c
james <james@syndeticlogic.org>
2013-04-22 07:58:52 -0400

    Added VariableByteCodeTest

---
+++ src/test/java/syndeticlogic/catena/utility/VariableByteCodeTest.java
@@ -0,0 +1,23 @@
+package syndeticlogic.catena.utility;
+
+import static org.junit.Assert.*;
+
+import java.util.Random;
+
+import org.junit.Test;
+
+public class VariableByteCodeTest {
+
+    @Test
+    public void test() {
+        int input;
+        VariableByteCode code = new VariableByteCode(0);
+        Random rand = new Random(1337);
+        for(int i =0 ; i < 1024*1024*128; i++) {
+            input = Math.abs(rand.nextInt());
+            code.setValue(input);
+            assertEquals(input, code.value());
+        }
+        
+    }
+}
