c47fe48cc68eb2105315fa2e3c2e7b47ad4fe37f
james <james@syndeticlogic.org>
2013-04-23 14:45:14 -0400

    Fixed InvertedListDescriptor decode and some refactoring

--- src/main/java/syndeticlogic/catena/text/InvertedListDescriptor.java
+++ src/main/java/syndeticlogic/catena/text/InvertedListDescriptor.java
@@ -1,28 +1,29 @@
 package syndeticlogic.catena.text;
 
-import java.io.Serializable;
+import syndeticlogic.catena.type.Type;
+import syndeticlogic.catena.type.Codeable;
+import syndeticlogic.catena.utility.Codec;
 
-@SuppressWarnings("serial")
-public class InvertedListDescriptor implements Serializable {
-    private final String word;
-    private final long offset;
-    private final int length;
-    private final int documentFrequency;
+public class InvertedListDescriptor implements Codeable {
+    private String word;
+    private long fileOffset;
+    private int length;
+    private int documentFrequency;
 
     public InvertedListDescriptor(String word, long offset, int length, int documentFrequency) {
         this.word = word;
-        this.offset = offset;
+        this.fileOffset = offset;
         this.length = length;
         this.documentFrequency = documentFrequency;
     }
-
+    
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
         result = prime * result + documentFrequency;
         result = prime * result + length;
-        result = prime * result + (int) (offset ^ (offset >>> 32));
+        result = prime * result + (int) (fileOffset ^ (fileOffset >>> 32));
         result = prime * result + ((word == null) ? 0 : word.hashCode());
         return result;
     }
@@ -40,7 +41,7 @@ public class InvertedListDescriptor implements Serializable {
             return false;
         if (length != other.length)
             return false;
-        if (offset != other.offset)
+        if (fileOffset != other.fileOffset)
             return false;
         if (word == null) {
             if (other.word != null)
@@ -55,7 +56,7 @@ public class InvertedListDescriptor implements Serializable {
     }
 
     public long getOffset() {
-        return offset;
+        return fileOffset;
     }
 
     public int getLength() {
@@ -64,5 +65,51 @@ public class InvertedListDescriptor implements Serializable {
     
     public int getDocumentFrequency() {
         return documentFrequency;
+    }
+
+    @Override
+    public int size() {
+        return 2*Type.INTEGER.length()+Type.LONG.length()+Type.STRING.length()+word.length();
+    }
+   
+    @Override
+    public String oridinal() {
+        throw new RuntimeException("Unsupported");
+    }
+
+    @Override
+    public int encode(byte[] dest, int offset) {
+        int copied = 0;
+        copied += Codec.getCodec().encode(word, dest, offset);
+        copied += Codec.getCodec().encode(this.fileOffset, dest, offset+copied); 
+        copied += Codec.getCodec().encode(length, dest, offset+copied); 
+        copied += Codec.getCodec().encode(documentFrequency, dest, offset+copied); 
+        return copied;
+    }
+
+    @Override
+    public int decode(byte[] source, int offset) {
+        int decoded = 0;
+        word = Codec.getCodec().decodeString(source, offset);
+        decoded += Type.STRING.length()+word.length();
+        this.fileOffset = Codec.getCodec().decodeLong(source, offset+decoded);
+        decoded += Type.LONG.length();
+        length = Codec.getCodec().decodeInteger(source, offset+decoded);
+        decoded += Type.INTEGER.length();
+        documentFrequency = Codec.getCodec().decodeInteger(source, offset+decoded);
+        decoded += Type.INTEGER.length();
+        return decoded;
+    }
+
+    @Override
+    public int compareTo(Codeable c) {
+        InvertedListDescriptor other = (InvertedListDescriptor)c;
+        if(documentFrequency < other.documentFrequency) {
+            return -1;
+        } else if(documentFrequency == other.documentFrequency) {
+            return 0;
+        } else {
+            return 1;
+        }
     }
 }
